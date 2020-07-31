f22dbed2a449f90f1fb933a0579806edb077628b
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-07-31 05:19:36 -0700

    some changes inside signup

--- src/components/signup.js
+++ src/components/signup.js
@@ -244,9 +244,10 @@ export default class Signup extends Component {
     const {selectedM} = this.state;
     const {selectedS} = this.state;
 
+
 // handle button click of signin form
 const handleSignin = () => {
-    axios.get('http://127.0.0.1:5002/job/singup/', {params : {fisrtname : this.state.fisrtname,lastname : this.state.lastname,email : this.state.email,password : this.state.password,sexe : this.state.sexe,age : this.state.age,country : this.state.country,telephone : this.state.telephone,majors : this.state.selectedM.toString(),skills : this.state.selectedS.toString()}})
+    axios.get('http://127.0.0.1:5002/job/singup/', {params : {fisrtname : this.state.fisrtname,lastname : this.state.lastname,email : this.state.email,password : this.state.password,sexe : this.state.sexe,age : this.state.age,country : this.state.country,telephone : this.state.telephone}})
     .then(response =>  {
       // setter
       //this.setState({errorMessage: response.data});
