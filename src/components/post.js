84089424b65157eb2538d18d8bee585916eaaed9
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-04 04:34:56 -0700

    some changes inside post

--- src/components/post.js
+++ src/components/post.js
@@ -6,21 +6,20 @@ const listItems = (elements) => {
     return ( elements.map((element) => 
     <div className="card card-home mb-3">
       <div class="card-body">
-        <h5 class="card-title">{element[3]}</h5>
-        <p class="card-text">{element[0]}</p>
-        <Link to={props.link + element[0]} className="btn btn-primary">More</Link>
+        <h6 class="card-title text-sm">{element[1]}</h6>
+        <p class="text-sm text-muted">{element[0]}</p>
+        <Link to={props.link + element[0]} className="btn btn-sm btn-info">More</Link>
       </div>
     
     </div>
 ));
 }
 
-return <div>
+return <div className="container">
 
-<div className="container">
-<p className=" text-sm-left">{props.name}</p>
+
 <div className="col-md-12 mb-5">
-
+<p className=" text-sm-left">{props.name}</p>
 <div className="card-deck mb-1">
     {listItems(props.elements.slice(0,5))}
 </div>
@@ -30,8 +29,6 @@ return <div>
 </div>
 
 </div>
-</div>
-
 </div>;
 
 }
