4fd45f715ff05006fba4cdb6deef5c8efaa03cb0
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-05 14:22:44 -0700

    add some picturs & company & nav

---
+++ src/components/company.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import Post from "./post";
+
+// handle button click of login form
+async function ifAbonner(company) {
+  try {
+    const response = await axios.get('http://127.0.0.1:5002/job/check/abonnement/user/' + localStorage.getItem('email') + '/company/' + company);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// handle button click of login form
+async function jobsByCompany(company) {
+    try {
+      const response = await axios.get('http://127.0.0.1:5002/job/abonnements/company/' + company);
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+export default class Company extends Component {
+
+state = { checked: false,enable: false,jobs: [] }
+
+
+componentDidMount =()=>{
+
+  ifAbonner(this.props.match.params.company).then(response => {
+if(response.data === 'yes'){
+  this.setState({
+    enable: true
+  });
+}
+  });
+  jobsByCompany(this.props.match.params.company).then(response => {
+	  this.setState({
+		jobs: response.data
+	  });
+	  console.log('company : ' + this.props.match.params.company)
+	});
+  }
+
+
+render() {
+
+// handle button click of login form
+const new_Abonnement = () => {
+    axios.get('http://127.0.0.1:5002/job/abonnement/new/email/' +  localStorage.getItem('email') + '/company/' +  this.props.match.params.company)
+    .then(function (response) {
+      // setter
+      //const token = localStorage.setItem('token', response.data.access_token)
+      //const user = localStorage.setItem('user', response.data.user)
+      // route for profile
+      console.log(response)
+
+    }).catch(function (error) {
+      console.log('ibrahim => ' + error);
+    });
+
+}
+
+
+// handle button click of login form
+const delete_Abonnement = () => {
+    axios.get('http://127.0.0.1:5002/job/abonnement/delete', {params : {_email:  localStorage.getItem('email'),  _company: this.props.match.params.company}})
+    .then(function (response) {
+      // setter
+      //const token = localStorage.setItem('token', response.data.access_token)
+      //const user = localStorage.setItem('user', response.data.user)
+      // route for profile
+      console.log(response)
+
+    }).catch(function (error) {
+      console.log('ibrahim => ' + error);
+    });
+}
+
+const tags = ['adventure','animation','children','comedy','fantasy','children','fantasy','romance','drama','action','crime','thriller','horror','mystery','sci-Fi','documentary','imax'];
+    
+
+return (<div className="container">
+
+<div className="card mb-5 border-0 rounded-0 shadow-sm bg-light">
+	<div className="row no-gutters">
+		<div className="col-md-3">
+			<img src="/undraw_business_decisions_gjwy.svg" class="w-100 h-100 mt-2 rounded p-3" alt="..." />
+		</div>
+		<div className="col-md-9">
+			<div className="card-body">
+				<h5 className="card-title">{ this.props.match.params.company }</h5>
+      
+  {
+  this.state.enable
+    ? <p className="card-text"><button type="submit" className="btn btn-sm btn-secondary disabled" onClick={delete_Abonnement}>s'abonner</button></p>
+    : <p className="card-text"><button type="submit" className="btn btn-sm btn-danger" onClick={new_Abonnement}>s'abonner</button></p>
+  }  
+  		</div>
+		</div>
+	</div>
+</div>
+
+<Post name="all jobs" link="/single/" elements={this.state.jobs}></Post>
+
+</div>
+);
+}
+
+}
---
+++ src/components/nav.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav(props){
+
+return <div>
+
+<nav className="navbar navbar-expand-sm navbar-light shadow-sm mb-3 mt-3">
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+	  <Link className="nav-link" to={"/"}>Home <span class="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="aa" tabindex="-1" aria-disabled="true">/</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="aa" tabindex="-1" aria-disabled="true">{props.name}</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+</div>;
+
+}
+
+export default Nav;
