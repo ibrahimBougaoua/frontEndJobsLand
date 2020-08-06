96528ac447bf596b4a1ce1900cc15d6efd649d64
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-06 10:19:31 -0700

    some changes inside interesse

--- src/components/interesse.js
+++ src/components/interesse.js
@@ -32,17 +32,14 @@ export default class Interesse extends Component {
 render() {
 
 const listOfJobs = this.state.jobs.map((element) => 
-<div className="col-md-12">
-<div className="card mb-5 border-0 rounded-0 shadow-sm bg-light">
-<div className="row no-gutters">
-    <div className="col-md-10">
-        <div className="card-body">
-            <small className="card-title">{element[5]}</small><br />
-            <small className="text-muted">{element[1]}</small><br />
-            <Link className="btn btn-primary btn-sm position-relative" to={"/single/" + element[10]} style={{top: "15px"}}>Show more</Link>
-        </div>
-    </div>
-</div>
+<div className="col-md-3">
+<div className="card border-0 mr-2 shadow-sm mb-3 bg-white rounded">
+  <div className="card-body">
+    <h6 className="card-subtitle mb-3 text-dark w-100">{ element[5].length < 50 ? element[5] : element[5].substring(0, 50) + '...' }</h6>
+    <p className="card-subtitle mb-3 text-muted w-100">company : { element[2].length < 15 ? element[2] : element[2].substring(0, 15) + '...' }</p>
+    <p className="card-subtitle mb-3 text-muted">published : {element[1]}</p>
+    <Link to={'/single/' + element[10]} className="btn btn-sm btn-outline-info">Read more</Link>
+  </div>
 </div>
 </div>
 );
