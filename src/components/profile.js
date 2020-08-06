268f67a17d63520ccd35c665df735cbb5cce032f
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-06 11:39:32 -0700

    some changes inside profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import MultiSelect from "@khanacademy/react-multi-select";
 
 // handle button click of login form
 async function getUserInformation() {
@@ -12,190 +11,6 @@ async function getUserInformation() {
     console.error(error);
   }
 }
-
-const optionsM = [
-{label: "Java Developer", value: "Java Developer"},            
-{label: "Software Developer", value: "Software Developer"},         
-{label: "Software Engineer", value: "Software Engineer"},          
-{label: "Web Developer", value: "Web Developer"},              
-{label: "PHP Developer", value: "PHP Developer"},              
-{label: "Graphic Designer", value:  "Graphic Designer"},           
-{label: "Software QA Engineer", value: "Software QA Engineer"},       
-{label: ".NET Developer", value: ".NET Developer"},             
-{label: "Android Developer", value: "Android Developer"},          
-{label: "Database Admin/Dev", value: "Database Admin/Dev"},         
-{label: "Programmer", value: "Programmer"},                 
-{label: "IT Specialist", value: "IT Specialist"},              
-{label: "iOS Developer", value: "iOS Developer"},              
-{label: "Senior Web Developer", value: "Senior Web Developer"},       
-{label: "Java Software Developer", value: "Java Software Developer"},    
-{label: "Network Administrator", value: "Network Administrator"},      
-{label: "Software Engineer", value: "Software Engineer"},          
-{label: "C++ Software Developer", value: "C++ Software Developer"},     
-{label: "Credit Specialist", value: "Credit Specialist"},          
-{label: "IT Manager", value: "IT Manager"},                 
-{label: "ASP.NET Developer", value: "ASP.NET Developer"},
-];
-
-const optionsS = [
-{label: '.Net', value: ".Net"},
-{label: '3G', value: "3G"},
-{label: 'ACH', value: "ACH"},
-{label: 'AI', value: "AI"},
-{label: 'AJAX', value: "AJAX"},
-{label: 'Accounting', value: "Accounting"},
-{label: 'Active Directory', value: "Active Directory"},
-{label: 'Adobe InDesign', value: "Adobe InDesign"},
-{label: 'Adobe Photoshop', value: "Adobe Photoshop"},
-{label: 'Agriculture', value: "Agriculture"},
-{label: 'Android', value: "Android"},
-{label: 'Angular', value: "Angular"},
-{label: 'Ant', value: "Ant"},
-{label: 'Apache', value: "Apache"},
-{label: 'Azure', value: "Azure"},
-{label: 'BGP', value: "BGP"},
-{label: 'Bootstrap', value: "Bootstrap"},
-{label: 'Business Analysis', value: "Business Analysis"},
-{label: 'C#', value: "C#"},
-{label: 'C/C++', value: "C/C++"},
-{label: 'CAD', value: "CAD"},
-{label: 'CCNA', value: "CCNA"},
-{label: 'CCNP', value: "CCNP"},
-{label: 'CI', value: "CI"},
-{label: 'CSS', value: "CSS"},
-{label: 'Cassandra', value: "Cassandra"},
-{label: 'CentOS', value: "CentOS"},
-{label: 'Cisco Routers', value: "Cisco Routers"},
-{label: 'CompTIA Network+', value: "CompTIA Network+"},
-{label: 'Content Management Systems', value: "Content Management Systems"},
-{label: 'Crystal Reports', value: "Crystal Reports"},
-{label: 'DB2', value: "DB2"},
-{label: 'DHCP', value: "DHCP"},
-{label: 'DNS', value: "DNS"},
-{label: 'Database Administration', value: "Database Administration"},
-{label: 'Design Patterns', value: "Design Patterns"},
-{label: 'Digital Marketing', value: "Digital Marketing"},
-{label: 'Drupal', value: "Drupal"},
-{label: 'E-Commerce', value: "E-Commerce"},
-{label: 'Eclipse', value: "Eclipse"},
-{label: 'Elasticsearch', value: "Elasticsearch"},
-{label: 'Ember', value: "Ember"},
-{label: 'Erlang', value: "Erlang"},
-{label: 'Excel', value: "Excel"},
-{label: 'FTP', value: "FTP"},
-{label: 'Firewall', value: "Firewall"},
-{label: 'GIS', value: "GIS"},
-{label: 'Git', value: "Git"},
-{label: 'Go', value: "Go"},
-{label: 'Google Analytics', value: "Google Analytics"},
-{label: 'HBase', value: "HBase"},
-{label: 'HTML5', value: "HTML5"},
-{label: 'HTTPS', value: "HTTPS"},
-{label: 'Hadoop', value: "Hadoop"},
-{label: 'Hibernate', value: "Hibernate"},
-{label: 'IIS', value: "IIS"},
-{label: 'ITIL', value: "ITIL"},
-{label: 'IVR', value: "IVR"},
-{label: 'JBoss', value: "JBoss"},
-{label: 'JSON', value: "JSON"},
-{label: 'Java', value: "Java"},
-{label: 'JavaScript', value: "JavaScript"},
-{label: 'Kanban', value: "Kanban"},
-{label: 'Linux', value: "Linux"},
-{label: 'Lucene', value: "Lucene"},
-{label: 'MCITP', value: "MCITP"},
-{label: 'MCSA', value: "MCSA"},
-{label: 'MCSD', value: "MCSD"},
-{label: 'MCSE', value: "MCSE"},
-{label: 'Mac OS', value: "Mac OS"},
-{label: 'Magento', value: "Magento"},
-{label: 'Marketing', value: "Marketing"},
-{label: 'Marketing Automation', value: "Marketing Automation"},
-{label: 'Marketo', value: "Marketo"},
-{label: 'Maven', value: "Maven"},
-{label: 'Maya', value: "Maya"},
-{label: 'Microsoft Access', value: "Microsoft Access"},
-{label: 'Microsoft Office', value: "Microsoft Office"},
-{label: 'Microsoft SQL Server', value: "Microsoft SQL Server"},
-{label: 'Microsoft Windows Server', value: "Microsoft Windows Server"},
-{label: 'MongoDB', value: "MongoDB"},
-{label: 'MySQL', value: "MySQL"},
-{label: 'NFS', value: "NFS"},
-{label: 'Network', value: "Network"},
-{label: 'Network Engineering', value: "Network Engineering"},
-{label: 'NoSQL', value: "NoSQL"},
-{label: 'Node.js', value: "Node.js"},
-{label: 'OOP', value: "OOP"},
-{label: 'OSPF', value: "OSPF"},
-{label: 'Objective-C', value: "Objective-C"},
-{label: 'Oracle', value: "Oracle"},
-{label: 'PCI', value: "PCI"},
-{label: 'PHP', value: "PHP"},
-{label: 'PL/SQL', value: "PL/SQL"},
-{label: 'Perforce', value: "Perforce"},
-{label: 'Perl', value: "Perl"},
-{label: 'PostGIS', value: "PostGIS"},
-{label: 'PostgreSQL', value: "PostgreSQL"},
-{label: 'PowerShell', value: "PowerShell"},
-{label: 'Project Implementation', value: "Project Implementation"},
-{label: 'Python', value: "Python"},
-{label: 'R', value: "R"},
-{label: 'REST', value: "REST"},
-{label: 'RN', value: "RN"},
-{label: 'React', value: "React"},
-{label: 'Redis', value: "Redis"},
-{label: 'Ruby', value: "Ruby"},
-{label: 'S3', value: "S3"},
-{label: 'SAP', value: "SAP"},
-{label: 'SAS', value: "SAS"},
-{label: 'SDLC', value: "SDLC"},
-{label: 'SEO', value: "SEO"},
-{label: 'SOAP', value: "SOAP"},
-{label: 'SPSS', value: "SPSS"},
-{label: 'SQL', value: "SQL"},
-{label: 'SQLite', value: "SQLite"},
-{label: 'SSH', value: "SSH"},
-{label: 'SSL', value: "SSL"},
-{label: 'SVN', value: "SVN"},
-{label: 'SaaS', value: "SaaS"},
-{label: 'Scala', value: "Scala"},
-{label: 'Scripting', value: "Scripting"},
-{label: 'Selenium', value: "Selenium"},
-{label: 'Service-Oriented Architecture', value: "Service-Oriented Architecture"},
-{label: 'SharePoint', value: "SharePoint"},
-{label: 'Software Development', value: "Software Development"},
-{label: 'Software Testing', value: "Software Testing"},
-{label: 'Spring', value: "Spring"},
-{label: 'Struts', value: "Struts"},
-{label: 'Swift', value: "Swift"},
-{label: 'Sybase', value: "Sybase"},
-{label: 'System Administration', value: "System Administration"},
-{label: 'T-SQL', value: "T-SQL"},
-{label: 'TCP', value: "TCP"},
-{label: 'TCP/IP', value: "TCP/IP"},
-{label: 'TFS', value: "TFS"},
-{label: 'Technical Support', value: "Technical Support"},
-{label: 'Telecommunication', value: "Telecommunication"},
-{label: 'Threading', value: "Threading"},
-{label: 'Tomcat', value: "Tomcat"},
-{label: 'Troubleshooting', value: "Troubleshooting"},
-{label: 'UI', value: "UI"},
-{label: 'UX', value: "UX"},
-{label: 'Ubuntu', value: "Ubuntu"},
-{label: 'Unit Testing', value: "Unit Testing"},
-{label: 'VMWare', value: "VMWare"},
-{label: 'Visio', value: "Visio"},
-{label: 'Visual Basic', value: "Visual Basic"},
-{label: 'Visual Studio', value: "Visual Studio"},
-{label: 'Web Development', value: "Web Development"},
-{label: 'WebSphere', value: "WebSphere"},
-{label: 'Windows', value: "Windows"},
-{label: 'WordPress', value: "WordPress"},
-{label: 'XML', value: "XML"},
-{label: 'XSLT', value: "XSLT"},
-{label: 'Xero', value: "Xero"},
-{label: 'iOS', value: "iOS"},
-];
 
 export default class Profile extends Component {
     
@@ -261,9 +76,6 @@ export default class Profile extends Component {
 
     render() {
 
-    const {selectedM} = this.state;
-    const {selectedS} = this.state;
-
 // handle button click of login form
 const handleUpdateUserInformation = () => {
   axios.get('http://localhost:5000/movie/user/update/' + localStorage.getItem('email'), {params : {fisrtname : this.state.fisrtname,lastname : this.state.lastname,email : this.state.email,password : this.state.password,sex : this.state.sex,age : this.state.age,country : this.state.country,telephone : this.state.telephone}})
@@ -324,17 +136,7 @@ return (
                         </div>
 
                         <div className="form-group  col-md-6">
-                            <label for="name">Majors</label>
-                            <MultiSelect options={optionsM} selected={selectedM} onSelectedChanged={selectedM => this.setState({selectedM})} />
-                        </div>
-
-                        <div className="form-group  col-md-6">
-                            <label for="name">Skills</label>
-                            <MultiSelect options={optionsS} selected={selectedS} onSelectedChanged={selectedS => this.setState({selectedS})} />
-                        </div>
-
-                        <div className="form-group  col-md-6">
-                            <label for="name">Sex</label>
+                            <label for="name">Sexe</label>
                             <select name="sexe" class="form-control" value={this.state.sexe} onChange={this.handleChangeSexe}>
                                 <option value="male">male</option>
                                 <option value="female">female</option>
