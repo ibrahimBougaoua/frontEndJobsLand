0620367992af59bfac4454468a7ebed81c3fe55e
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-08 03:12:15 -0700

    some changes inside resultat

--- src/components/resultat.js
+++ src/components/resultat.js
@@ -4,22 +4,16 @@ import { Link } from "react-router-dom";
 function Resultat(props){
 
 const listItems = props.elements.map((element) =>
-<div className="card mb-5 border-0 rounded-0 shadow-sm bg-light">
-	<div className="row no-gutters">
-	<div className="col-md-3">
-		<img src={element[13]} class="card-img rounded-0" alt="..." />
-	</div>
-	<div className="col-md-9">
-	<div className="card-body">
-	<h5 className="card-title">{element[3]}</h5>
-	<small className="text-muted">{element[4]}</small><br />
-	<small className="text-muted">{element[11]}</small><br />
-	{element[7].split(',').map((element) => <Link className="btn btn-sm btn-outline-info mr-2 mt-2 mb-2" to={"/account/tag/" + element}>{element}</Link>)}<br />
-	<Link className="btn btn-sm btn-outline-info mr-2 mt-2 mb-2" to={"/movie/single/" + element[0]}>More</Link>
-	</div>
-	</div>
-	</div>
-	</div>
+<div className="col-md-3">
+<div className="card border-0 mr-2 shadow-sm mb-3 bg-white rounded">
+  <div className="card-body">
+    <h6 className="card-subtitle mb-3 text-dark w-100">{ element[5].length < 50 ? element[5] : element[5].substring(0, 50) + '...' }</h6>
+    <p className="card-subtitle mb-3 text-muted w-100">company : { element[2].length < 15 ? element[2] : element[2].substring(0, 15) + '...' }</p>
+    <p className="card-subtitle mb-3 text-muted">published : {element[1]}</p>
+    <Link to={'/single/' + element[10]} className="btn btn-sm btn-outline-info">Read more</Link>
+  </div>
+</div>
+</div>
 );
 
 return <div className="container mt-3">
